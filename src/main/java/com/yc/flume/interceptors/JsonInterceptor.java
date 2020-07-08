ba876dbdf7106d1cc91432290a4847aeb8a271a9
zhangyingchengqi <1069595532@qq.com>
2020-07-08 22:19:45 +0800

    加入日志采集

---
+++ src/main/java/com/yc/flume/interceptors/JsonInterceptor.java
@@ -0,0 +1,88 @@
+package com.yc.flume.interceptors;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JsonInterceptor implements Interceptor {
+
+    private String[] schema; //  ["id","name","age"]
+    private String separator; //,
+
+    public JsonInterceptor(String schema, String separator) {
+        this.schema = schema.split("[,]");
+        this.separator = separator;
+    }
+
+    @Override
+    public void initialize() {
+        //no-op
+    }
+
+    @Override
+    public Event intercept(Event event) {
+        Map<String, String> tuple = new LinkedHashMap();
+        //将传入的Event中的body内容，加上schema，然后在放入到Event
+        String line = new String(event.getBody());
+        String[] fields = line.split(separator);
+        for(int i = 0; i < schema.length; i++) {   //schema:   ["id","name","age"]
+            String key = schema[i];
+            String value = fields[i];
+            tuple.put(key, value);   //
+        }
+        //
+        String json = JSONObject.toJSONString(tuple);
+        //将转换好的json，再放入到Event中
+        event.setBody(json.getBytes());
+        return event;
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> events) {
+        for (Event e: events) {
+            intercept(e);
+        }
+        return events;
+    }
+
+    @Override
+    public void close() {
+        //no-op
+    }
+
+
+    /**
+     * Interceptor.Builder的生命周期方法
+     * 构造器 -> configure -> build
+     */
+    public static class Builder implements Interceptor.Builder {
+
+        private String fields;
+        private String separator;
+
+        @Override
+        public Interceptor build() {
+            //在build创建JsonInterceptor的实例
+            return new JsonInterceptor(fields, separator);
+        }
+
+        /**
+         * 配置文件中应该有哪些属性？
+         * 1.数据的分割符
+         * 2.字段名字（schema）
+         * 3.schema字段的分隔符
+         * @param context
+         */
+        @Override
+        public void configure(Context context) {
+            fields = context.getString("fields");    // a1.sources.r1.interceptors.i1.fields=id,name,age
+            separator = context.getString("separator");  //a1.sources.r1.interceptors.i1.separator=,
+        }
+    }
+}
+
